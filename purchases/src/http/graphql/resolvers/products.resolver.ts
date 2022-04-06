6c5edf8a813c68d9fc408b6904a462251edfa264
João Gomes <joao.gomes@unizon.pt>
2022-04-06 17:19:10 +0100

    Create Product

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,7 +1,8 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthorizationGuard } from '../../../http/auth/authorization.guard';
 import { ProductsService } from '../../../services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
 import { Product } from '../models/product';
 
 @Resolver()
@@ -9,8 +10,13 @@ export class ProductsResolver {
   constructor(private productsService: ProductsService) {}
 
   @Query(() => [Product])
-  @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.index();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.create(data);
   }
 }
--- purchases/src/services/products.service.ts
+++ purchases/src/services/products.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
 import { PrismaService } from '../database/prisma/prisma.service';
+
+interface CreateProductParams {
+  title: string;
+}
 
 @Injectable()
 export class ProductsService {
@@ -7,5 +12,26 @@ export class ProductsService {
 
   index() {
     return this.prisma.product.findMany();
+  }
+
+  async create({ title }: CreateProductParams) {
+    const slug = slugify(title, {
+      lower: true,
+    });
+
+    const productWithSameSlug = await this.prisma.product.findUnique({
+      where: { slug },
+    });
+
+    if (productWithSameSlug) {
+      throw new Error('Another product with same slug already exists');
+    }
+
+    return await this.prisma.product.create({
+      data: {
+        title,
+        slug,
+      },
+    });
   }
 }
