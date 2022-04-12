ff27e8da7bbb62dc282e285a6850e922060441d8
João Gomes <joao.gomes@unizon.pt>
2022-04-12 22:04:10 +0100

    Update dev log

--- purchases/src/main.ts
+++ purchases/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3333);
+  await app.listen(3333).then(() => {
+    console.log('[Purchases] HTTP and Microservices Servers running');
+  });
 }
 bootstrap();
