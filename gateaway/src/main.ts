9428055560f5189000beaa8a4282d71d66370dc8
João Gomes <joao.gomes@unizon.pt>
2022-04-12 22:04:47 +0100

    Update dev log

--- gateaway/src/main.ts
+++ gateaway/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3332);
+  await app.listen(3332).then(() => {
+    console.log('[Gateway] HTTP Server running');
+  });
 }
 bootstrap();
